a497ce91660d8d1e7d470f8655684aeaf8a995ed
Ali Eren Aydın <aere612gmail.com>
2023-04-04 11:22:58 +0300

    refactor: Projects merged and interactions refactored

---
+++ Assets/Scripts/Interacted.cs
@@ -0,0 +1,5 @@
+
+public interface Interacted
+{
+    public void Interaction();
+}
--- Assets/Scripts/interactions.cs
+++ Assets/Scripts/interactions.cs
@@ -1,41 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class interactions : MonoBehaviour
+public class Interactions : MonoBehaviour, IPointerDownHandler
 {
     int layerMask;
-    RaycastHit hit;
     public GameObject Player, ScreenCam, PlayerCam;
+
     private void Start()
     {
         layerMask = 1 << 2;
         layerMask = ~layerMask;
     }
-    void Update()
+
+    public virtual void OnPointerDown(PointerEventData pointerData)
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 3, layerMask);
-            try { 
-                if (hit.transform.CompareTag("Screen"))
-                {
-                    Cursor.visible = true;
-                    Cursor.lockState = CursorLockMode.None;
-                    ScreenCam.SetActive(true);
-                    PlayerCam.SetActive(false);
-                    Player.GetComponent<pm>().enabled = false;
-                }
-                else if (hit.transform.CompareTag("J_Door"))
-                {
-                    hit.transform.GetComponent<J_Door>().triggered = true;
-                }
-                else if (hit.transform.CompareTag("DeadBolt"))
-                {
-                    hit.transform.GetComponent<DeadBolt>().triggered = true;
-                }
-            }
-            catch {}
-        }
+        Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out var hit, 3, layerMask);
+        if (!hit.collider.TryGetComponent<Interacted>(out var interactedObject)) return;
+        interactedObject.Interaction();
     }
 }
