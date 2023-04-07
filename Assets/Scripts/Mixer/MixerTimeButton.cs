8eadebd2e26e50f34bdf0e54db04dfdd67da69f8
Altay Turan <69851770+altayturan@users.noreply.github.com>
2023-04-07 16:59:25 +0300

    fix: Mixer buttons reseting time fixed

--- Assets/Scripts/Mixer/MixerTimeButton.cs
+++ Assets/Scripts/Mixer/MixerTimeButton.cs
@@ -6,6 +6,7 @@ public class MixerTimeButton : MonoBehaviour, IInteractable
     [SerializeField] private int _countdown;
     public void Interaction()
     {
+        if (_mixerController.isCountdownStarted) return;
         _mixerController.currentCountdown = _countdown;
         _mixerController.isCountdownStarted = true;
     }
