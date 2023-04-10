dcc91d52b8eeb518894a7551b2b3b7f2a5245051
Altay Turan <69851770+altayturan@users.noreply.github.com>
2023-04-10 11:48:19 +0300

    feat: Litter bin added

--- Assets/Scripts/Objects/Objects/BaseObject.cs
+++ Assets/Scripts/Objects/Objects/BaseObject.cs
@@ -7,7 +7,7 @@ public class BaseObject : MonoBehaviour
     [SerializeField] internal BaseObjectSlot _objectSlot;
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.collider.TryGetComponent<Sink>(out var sink))
+        if (collision.collider.TryGetComponent<LitterBin>(out var sink))
         {
             Destroy(this.gameObject,0.1f);
         }
