d900996299d1cf597944328556fbea1084821317
Ali Eren Aydın <aere612gmail.com>
2023-04-10 10:30:57 +0300

    feat: Study door added

--- Assets/Scripts/S_Door.cs
+++ Assets/Scripts/S_Door.cs
@@ -1,21 +1,21 @@
 using UnityEngine;
 using DG.Tweening;
 
-public class J_Door : MonoBehaviour, IInteractable
+public class S_Door : MonoBehaviour, IInteractable
 {
     [SerializeField] private bool doorClosed = true;
-    [SerializeField] private Deadbolt deadbolt;
+    [SerializeField] private bool isLocked = true;
     [SerializeField] private Transform pivot;
 
     public bool DoorClosed => doorClosed;
 
     public void Interaction()
     {
-        if (deadbolt.IsLocked) return;
+        if (isLocked) return;
         if (DoorClosed)
         {
             doorClosed = false;
-            pivot.DORotate(new Vector3(0, 90, 0f), 0.5f);
+            pivot.DORotate(new Vector3(0, -90, 0f), 0.5f);
             return;
         }
 
