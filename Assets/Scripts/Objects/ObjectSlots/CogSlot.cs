3f273c1c341fb041297c123f8cb0a99418728963
Ali Eren Aydın <aere612gmail.com>
2023-04-24 13:59:55 +0300

    fix: Cog sfx, puzzle books rigidbody deleted

--- Assets/Scripts/Objects/ObjectSlots/CogSlot.cs
+++ Assets/Scripts/Objects/ObjectSlots/CogSlot.cs
@@ -1,10 +1,13 @@
+using UnityEngine;
+
 public class CogSlot : BaseObjectSlot
 {
+    [SerializeField] private AudioSource cogInsertSfx;
     public override void Interaction()
     {
         if (_playerHandSo.CurrentObject == null) return;
         if (!_playerHandSo.CurrentObject.TryGetComponent<Cog>(out var _cog) || _currentObject != null) return;
-
+        cogInsertSfx.Play();
         _currentObject = _cog;
         PlaceTheObject(_currentObject.gameObject,false);
         _currentObject._objectSlot = this;
