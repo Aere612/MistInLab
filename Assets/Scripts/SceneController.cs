5d5588cb3282164b7bce715a9821edcfaf8cf027
Altay Turan <69851770+altayturan@users.noreply.github.com>
2023-04-26 15:51:20 +0300

    refactor: Papers refactored, volumes changed, Kobalt material changed

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -5,6 +5,7 @@ public class SceneController : MonoBehaviour
 {
     [SerializeField] private Counter counter;
     [SerializeField] private InputController inputController;
+    [SerializeField] private GameObject _monsterSpawner;
 
     private void Start()
     {
@@ -21,12 +22,14 @@ public class SceneController : MonoBehaviour
     [ContextMenu("LoseGame")]
     public void LoseTheGame()
     {
+        
         EndScene();
         SceneManager.LoadScene("LoseScene");
     }
 
     private void EndScene()
     {
+        _monsterSpawner.SetActive(false);
         inputController.CancelListening();
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
