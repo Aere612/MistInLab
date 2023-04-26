4eaab4b9e1045466120d6ea10c8eb8c48d328698
Altay Turan <69851770+altayturan@users.noreply.github.com>
2023-04-26 17:16:17 +0300

    refactor: Inspection tutorial refactored

--- Assets/Scripts/Tutorial/InspectTutorial.cs
+++ Assets/Scripts/Tutorial/InspectTutorial.cs
@@ -7,22 +7,22 @@ public class InspectTutorial : MonoBehaviour
     [SerializeField] private SpriteRenderer spriteRenderer;
     [SerializeField] private SpriteRenderer spriteRenderer2;
     [SerializeField] private InteractionTutorial interactionTutorial;
-    [SerializeField] private GameObject magnify;
+    [SerializeField] private GameObject arrowOne;
+    [SerializeField] private GameObject arrowTwo;
+    [SerializeField] private GameObject arrowThree;
+    [SerializeField] private GameObject arrowFour;
     private void Awake()
     {
-        magnify.SetActive(true);
+        arrowOne.SetActive(true);
+        arrowTwo.SetActive(true);
+        arrowThree.SetActive(true);
+        arrowFour.SetActive(true);
         var sequence = DOTween.Sequence();
         sequence.Append(transform.DOScale(0.12f, 1));
         sequence.Append(transform.DOScale(0.12f, 0.1f));
         sequence.Append(transform.DOScale(0.1f, 1));
         sequence.Append(transform.DOScale(0.1f, 0.1f));
         sequence.SetLoops(-1);
-        var sequence2 = DOTween.Sequence();
-        sequence2.Append(magnify.transform.DOScale(0.05f, 1));
-        sequence2.Append(magnify.transform.DOScale(0.05f, 0.1f));
-        sequence2.Append(magnify.transform.DOScale(0.04f, 1));
-        sequence2.Append(magnify.transform.DOScale(0.04f, 0.1f));
-        sequence2.SetLoops(-1);
     }
 
     private void Update()
