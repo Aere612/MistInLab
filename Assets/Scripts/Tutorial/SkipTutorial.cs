d63111a9c6501c782bb8cd45b16308c18ab8949c
kayrakocaeli <83376559+kayrakocaeli@users.noreply.github.com>
2023-12-14 19:44:52 +0300

    fix

--- Assets/Scripts/Tutorial/SkipTutorial.cs
+++ Assets/Scripts/Tutorial/SkipTutorial.cs
@@ -32,7 +32,7 @@ public class SkipTutorial : MonoBehaviour
     private void Skip()
     {
         characterController.enabled = false;
-        player.position = new Vector3(-5.37900019f, 1.7f, 0.54400003f);
+        //player.position = new Vector3(-5.37900019f, 1.7f, 0.54400003f);
         characterController.enabled = true;
         door1.Rotate(new Vector3(0,90,0));
         door2.Rotate(new Vector3(0,-90,0));
